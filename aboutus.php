32ef0f8ed6fec56b6f3c57ac54af3732047968f3
simoneliu2203 <31769955+simoneliu2203@users.noreply.github.com>
2017-09-27 16:01:56 -0400

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,11 @@
+<?php include("index.php"); ?>
+
+        <div align="center" id = "main">
+            <h1>About Seahawk Rent-A-Car</h1>
+                <p>Seahawk Rent-A-Car is bla bla bla.</p>
+            <h1>About Us</h1>
+            <p>Seahawk Rent-A-Car is currently owned by Simone, Yujiemi, Zach and Jacob.</p>
+            
+            <p>Thank you!</p>
+            
+        </div>
