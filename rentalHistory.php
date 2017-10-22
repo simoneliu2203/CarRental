f8feb2c0e229d1a67a64db5f5f1aa21eb8176d3c
simoneliu2203 <31769955+simoneliu2203@users.noreply.github.com>
2017-10-21 22:56:25 -0400

    Delete rentalHistory.php

--- rentalHistory.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include("header.php");
-
-?>
-
-<div style="text-align:right; margin-right:auto"><a href="logout.php" style="color:coral; font-size:18px;margin-right:5px">Logout?</a></div>
-<div style="text-align:left; margin-left:10px"><a href="customerMenu.php" style="color:blue; font-size:18px;margin-right:5px"> &#8678 Back to Customer Menu</a></div>
-
-<h5>rental info</h5>
