f8dbdffbc61c87584b690b2625a440c0ae86af96
simoneliu2203 <31769955+simoneliu2203@users.noreply.github.com>
2017-11-10 11:45:18 -0500

    Add files via upload

--- manageBooking.php
+++ manageBooking.php
@@ -14,14 +14,74 @@
 <div style="text-align:right; margin-right:20px; color: red">Login as: <?php echo $username?></div>
 <div style="text-align:left; margin-left:10px"><a href="customerMenu.php" style="color:blue; font-size:18px;margin-right:5px"> &#8678 Back to Customer Menu</a></div>
 
+<style>
+.button {
+    border: none;
+	border-radius: 12px;
+    color: white;
+    padding: 5px 18px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+
+.button1 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid green;
+}
+
+.button1:hover {
+    background-color: #4CAF50;
+    color: white;
+}
+
+.button2 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid darkorange;
+}
+
+.button2:hover {
+    background-color: orange;
+    color: white;
+}
+
+.button3 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid darkred;
+}
+
+.button3:hover {
+    background-color: red;
+    color: white;
+}
+</style>
+
 <h2 align="center">Bookings</h2>
 
 
 <?php 	
-	if(isset($_POST["submit"])){
- 		$selection = $_POST['selection'];
-		$id = $_POST['submit'];
-		$query = "update booking set status = '$selection' where booking_id = '$id'";
+	if(isset($_POST["approved"])){
+		$id = $_POST['approved'];
+		$query = "update booking set status = 'approved' where booking_id = '$id'";
+		mysqli_query($db, $query);
+	}
+	
+	if(isset($_POST["declined"])){
+		$id = $_POST['declined'];
+		$query = "update booking set status = 'declined' where booking_id = '$id'";
+		mysqli_query($db, $query);
+	}
+	
+	if(isset($_POST["cancelled"])){
+		$id = $_POST['cancelled'];
+		$query = "update booking set status = 'cancelled' where booking_id = '$id'";
 		mysqli_query($db, $query);
 	}
 ?>
@@ -41,7 +101,6 @@
 			<th width='10%' bgcolor=black style='color:white; padding:15px'>Total</th>
 			<th width='10%' bgcolor=black style='color:white; padding:15px'>Status</th>
 			<th width='10%' bgcolor=black style='color:white; padding:15px'>Option</th>
-			<th width='10%' bgcolor=black style='color:white; padding:15px'>Submit</th>
 			</tr>";
 		
 			$rented_car = mysqli_query($db,"SELECT * FROM get_quote natural join cars");
@@ -61,21 +120,16 @@
 					echo  "<td>" . "$". $row['total'] . "</td>";
 					echo  "<td>" . $row['status'] . "</td>"; 
 	?>	
+
 					<td>
-						  <select name="selection">
-							<option value="none">None</option>  
-							<option value="approved">Approved</option>
-							<option value="declined">Declined</option>
-						  </select>
-					</td>
-					<td>
-						<button type="submit" name="submit" value="<?php echo $row['booking_id']?>">Submit</button>
+						<button class="button button1" type="submit" name="approved" value="<?php echo $row['booking_id']?>">Approved</button>
+						<button class="button button2" type="submit" name="declined" value="<?php echo $row['booking_id']?>">Declined</button>
+						<button class="button button3" type="submit" name="cancelled" value="<?php echo $row['booking_id']?>">Cancelled</button>
 				   </td> 	
 				<?php						
 				}			
 			}
 ?>
 </form>
-
 
 
