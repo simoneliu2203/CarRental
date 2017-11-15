8f088f3c7968a4b2a9aecf8e184c1c258a005f54
simoneliu2203 <31769955+simoneliu2203@users.noreply.github.com>
2017-11-15 15:21:21 -0500

    Add files via upload

--- aboutus.php
+++ aboutus.php
@@ -33,9 +33,9 @@
 <!-- Description of the rental website --> 
 <div align="center" id = "main">
 	<h1>About Seahawk Rent-A-Car</h1>
-		<p>Seahawk Rent-A-Car is bla bla bla.</p>
+		<font size="4.5">Seahawk Rent-A-Car aims to revolutionize the car rental industry by providing an updated, streamlined software to manage the car rental process and all of the details it involves. Through seamless integration of client, inventory, and business operations databases, our product will provide a user experience that is fast and easy to use on both the client and administrative ends, while constantly providing the rental agency with up-to-date information on daily operations.</font>
 	<h1>About Us</h1>
-	<p>Seahawk Rent-A-Car is currently owned by Simone, Yujiemi, Leo.</p>
-
-	<p>Thank you!</p>
+	<font size="4.5">Seahawk Rent-A-Car is currently owned by Simone "Y" Liu, Yujiemi Chisholm, and Yasin Emre Gokce.</font>
+    
+	<p><font size="4.5">Thank you!</font>
 </div>
