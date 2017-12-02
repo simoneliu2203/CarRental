2f0b83609a882df3a157d42ee3d2a08b70f9ac9c
simoneliu2203 <31769955+simoneliu2203@users.noreply.github.com>
2017-12-02 15:01:14 -0500

    Add files via upload

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-//this connects the php code to the mysql databases (called 'login') using a localhost server
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'login');
+//this connects the php code to the mysql databases (called 'narayan10') using a webdev server
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'YOUR USERNAME GOES HERE');
+define('DB_PASSWORD', 'YOUR PASSWORD GOES HERE');
+define('DB_DATABASE', 'narayan10');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
 
